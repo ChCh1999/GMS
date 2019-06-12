4eeca7f372ecd23da1142456b70ff34733d42421
赵彪 <17354422502@163.com>
2019-06-13 01:22:03 +0800

    12
    对于MainRef修改了操作

--- src/teamUI/MainRefe.java
+++ src/teamUI/MainRefe.java
@@ -1,24 +1,12 @@
 package teamUI;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import java.awt.FlowLayout;
-import javax.swing.JDesktopPane;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JComboBox;
-import javax.swing.JTextArea;
-import javax.swing.JCheckBox;
-import javax.swing.JRadioButton;
-import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Enumeration;
 
 public class MainRefe extends JFrame {
 
@@ -63,7 +51,10 @@ public class MainRefe extends JFrame {
 		JScrollPane messagePane=new JScrollPane(messageArea,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		messagePane.setBounds(84, 16, 592, 139);
 		contentPane.add(messagePane);
-		
+
+
+
+
 		JLabel label_2 = new JLabel("性别");
 		label_2.setFont(new Font("宋体", Font.PLAIN, 18));
 		label_2.setBounds(21, 177, 53, 20);
@@ -72,7 +63,7 @@ public class MainRefe extends JFrame {
 		JPanel sex_panel = new JPanel();
 		sex_panel.setBounds(117, 165, 491, 39);
 		contentPane.add(sex_panel);
-		
+
 		JRadioButton radioB_man = new JRadioButton("男");
 		radioB_man.setFont(new Font("宋体", Font.PLAIN, 18));
 		sex_panel.add(radioB_man);
@@ -80,7 +71,14 @@ public class MainRefe extends JFrame {
 		JRadioButton radioB_woman = new JRadioButton("女");
 		radioB_woman.setFont(new Font("宋体", Font.PLAIN, 18));
 		sex_panel.add(radioB_woman);
-		
+
+		ButtonGroup bg_sex =new ButtonGroup();
+		bg_sex.add(radioB_man);
+		bg_sex.add(radioB_woman);
+
+
+
+
 		JLabel label_3 = new JLabel("年龄组");
 		label_3.setFont(new Font("宋体", Font.PLAIN, 18));
 		label_3.setBounds(19, 219, 64, 20);
@@ -101,53 +99,77 @@ public class MainRefe extends JFrame {
 		JRadioButton radioButton_4 = new JRadioButton("11-12");
 		radioButton_4.setFont(new Font("宋体", Font.PLAIN, 18));
 		age_panel.add(radioButton_4);
-		
+
+		ButtonGroup bg_age =new ButtonGroup();
+		bg_age.add(radioButton_2);
+		bg_age.add(radioButton_3);
+		bg_age.add(radioButton_4);
+
+
+
+
 		JLabel label_1 = new JLabel("比赛项目");
 		label_1.setFont(new Font("宋体", Font.PLAIN, 18));
 		label_1.setBounds(10, 266, 81, 20);
 		contentPane.add(label_1);
-		
+
+
 		JPanel game_panel = new JPanel();
 		game_panel.setLayout(null);
 		game_panel.setBounds(117, 266, 491, 81);
 		contentPane.add(game_panel);
-		
-		JCheckBox dangang = new JCheckBox("单杠");
+
+		JRadioButton dangang = new JRadioButton("单杠");
 		dangang.setBounds(6, 6, 56, 23);
 		game_panel.add(dangang);
-		
-		JCheckBox shuanggang = new JCheckBox("双杠");
+
+		JRadioButton shuanggang = new JRadioButton("双杠");
 		shuanggang.setBounds(97, 6, 56, 23);
 		game_panel.add(shuanggang);
-		
-		JCheckBox diaohuan = new JCheckBox("吊环");
+
+		JRadioButton diaohuan = new JRadioButton("吊环");
 		diaohuan.setBounds(187, 6, 63, 23);
 		game_panel.add(diaohuan);
-		
-		JCheckBox tiaoma = new JCheckBox("跳马");
+
+		JRadioButton tiaoma = new JRadioButton("跳马");
 		tiaoma.setBounds(6, 41, 56, 23);
 		game_panel.add(tiaoma);
-		
-		JCheckBox ticao = new JCheckBox("自由体操");
+
+		JRadioButton ticao = new JRadioButton("自由体操");
 		ticao.setBounds(292, 41, 80, 23);
 		game_panel.add(ticao);
-		
-		JCheckBox anma = new JCheckBox("鞍马");
+
+		JRadioButton anma = new JRadioButton("鞍马");
 		anma.setBounds(292, 6, 56, 23);
 		game_panel.add(anma);
-		
-		JCheckBox bengchuang = new JCheckBox("蹦床");
+
+		JRadioButton bengchuang = new JRadioButton("蹦床");
 		bengchuang.setBounds(402, 6, 63, 23);
 		game_panel.add(bengchuang);
-		
-		JCheckBox gaodigang = new JCheckBox("高低杠");
+
+		JRadioButton gaodigang = new JRadioButton("高低杠");
 		gaodigang.setBounds(97, 41, 69, 23);
 		game_panel.add(gaodigang);
-		
-		JCheckBox pinghengmu = new JCheckBox("平衡木");
+
+		JRadioButton pinghengmu = new JRadioButton("平衡木",true);
 		pinghengmu.setBounds(187, 41, 80, 23);
 		game_panel.add(pinghengmu);
-		
+
+		ButtonGroup bg_game=new ButtonGroup();
+		bg_game.add(dangang);
+		bg_game.add(shuanggang);
+		bg_game.add(diaohuan);
+		bg_game.add(tiaoma);
+		bg_game.add(ticao);
+		bg_game.add(anma);
+		bg_game.add(bengchuang);
+		bg_game.add(gaodigang);
+		bg_game.add(pinghengmu);
+
+
+
+
+
 		JLabel label_4 = new JLabel("预决赛");
 		label_4.setFont(new Font("宋体", Font.PLAIN, 18));
 		label_4.setBounds(21, 354, 64, 20);
@@ -164,16 +186,44 @@ public class MainRefe extends JFrame {
 		JRadioButton jRB_juesai = new JRadioButton("决赛");
 		jRB_juesai.setFont(new Font("宋体", Font.PLAIN, 18));
 		befinal_panel.add(jRB_juesai);
+
+		ButtonGroup bg_befinal =new ButtonGroup();
+		bg_befinal.add(jRB_yusai);
+		bg_befinal.add(jRB_juesai);
 		
 		JButton button = new JButton("比赛开始");
 		button.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
+
+				//获取通知栏
+				String textarea=messageArea.getText();
+
+				//获取各个选项的值
+				String sex=findthebuttonText(bg_sex);
+				String game=findthebuttonText(bg_game);
+				String age=findthebuttonText(bg_age);
+				String befinal=findthebuttonText(bg_befinal);
+
 				
 			}
 		});
 		button.setFont(new Font("宋体", Font.PLAIN, 18));
 		button.setBounds(278, 415, 145, 30);
 		contentPane.add(button);
+	}
+
+	//找到button组中的选项text值
+	public String findthebuttonText(ButtonGroup bg){
+		String Text=null;
+			Enumeration<AbstractButton> radioBtns=bg.getElements();
+		while (radioBtns.hasMoreElements()) {
+			AbstractButton btn = radioBtns.nextElement();
+			if(btn.isSelected()){
+				Text=btn.getText();
+				break;
+			}
+		}
+		return Text;
 	}
 }
